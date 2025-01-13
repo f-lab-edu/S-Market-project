d353d470acd8a2ce3810c8d859c73d39127ee6e7
songhee1 <ghenrhkwk88@naver.com>
2025-01-13 12:35:04 +0900

    프로젝트 환경설정

---
+++ src/test/java/com/flab/s_market/SMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.s_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
