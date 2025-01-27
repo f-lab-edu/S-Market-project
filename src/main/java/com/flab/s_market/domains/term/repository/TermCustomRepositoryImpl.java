93d74059bb0b6e7a77b9b2f944bdc5809288d611
songhee1 <ghenrhkwk88@naver.com>
2025-01-27 22:20:42 +0900

    [Refactor] 약관조회 서비스 코드 수정 (#1)
    - 약관 내용을 term테이블이 모두 들고있으므로 sub_term 테이블과 조인할 필요없어서 쿼리 제거
    - 버전의 내용은 필요하지 않으므로 서비스 클래스에서 version 필드 제거

--- src/main/java/com/flab/s_market/domains/term/repository/TermCustomRepositoryImpl.java
+++ src/main/java/com/flab/s_market/domains/term/repository/TermCustomRepositoryImpl.java
@@ -3,11 +3,9 @@ package com.flab.s_market.domains.term.repository;
 import com.flab.s_market.domains.term.domain.QSubTerm;
 import com.flab.s_market.domains.term.domain.QTerm;
 import com.flab.s_market.domains.term.domain.SubTerm;
-import com.flab.s_market.domains.term.domain.Term;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import java.util.List;
 import java.util.Optional;
 
 public class TermCustomRepositoryImpl implements TermCustomRepository{
@@ -15,20 +13,6 @@ public class TermCustomRepositoryImpl implements TermCustomRepository{
 
     public TermCustomRepositoryImpl(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
-    }
-
-    @Override
-    public List<Term> findTermByVersionWithJoin(Integer version) {
-        QTerm term = QTerm.term;
-        QSubTerm subTerm = QSubTerm.subTerm;
-
-        List<Term> terms = queryFactory
-            .select(subTerm.term)
-            .from(subTerm)
-            .join(subTerm.term, term)
-            .where(subTerm.id.version.eq(version))
-            .fetch();
-        return terms;
     }
 
     @Override
--- src/main/java/com/flab/s_market/domains/term/service/TermService.java
+++ src/main/java/com/flab/s_market/domains/term/service/TermService.java
@@ -19,19 +19,17 @@ public class TermService {
 
     private final TermRepository termRepository;
     private final SubTermRepository subTermRepository;
-    private static final Integer TERM_VERSION = 1;
     private static final String mainTitle = "이용약관에 먼저 동의해주세요";
     private static final String additionalTitle = "신규 가입 회원에게만 드려요.\\n 유니버스 크럽 3개월 무료 이용!";
     public AllTermResponseDTO getAllTerms(){
-        List<Term> terms = termRepository.findTermByVersionWithJoin(TERM_VERSION); //이상하다?? 최신
-        // 1. query 2. stream()
+        List<Term> terms = termRepository.findAll();
+
         List<TermResponseDTO> mainTermList =
             terms.stream()
                 .filter(term -> !term.getAdditional())
-//                .collect(Collectors.groupingBy()) // map리턴
                 .map(TermResponseDTO::of)
                 .toList();
-        // 신세계 멤버십
+
         List<TermResponseDTO> additionalTermList =
             terms.stream()
                 .filter(Term::getAdditional)
@@ -45,6 +43,6 @@ public class TermService {
     }
 
     public DetailTermDTO getDetailTerms() {
-        return subTermRepository.findByVersionAndUrl(TERM_VERSION);
+        return subTermRepository.findByVersionAndUrl();
     }
 }
