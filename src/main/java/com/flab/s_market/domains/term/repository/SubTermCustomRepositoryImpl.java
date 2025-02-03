ce0a0e4c7c31efba7c93768462e44db82dc1c811
songhee1 <ghenrhkwk88@naver.com>
2025-02-03 23:18:20 +0900

    [Refactor] 약관 상세조회 api 리펙터링 (#2)
    - 레포지토리로 데이터 조회시 dto 대신 엔티티로 반환받도록 변경
    - 엔티티를 dto로 변경할 수 있도록 static 메서드 추가

--- src/main/java/com/flab/s_market/domains/term/repository/SubTermCustomRepositoryImpl.java
+++ src/main/java/com/flab/s_market/domains/term/repository/SubTermCustomRepositoryImpl.java
@@ -1,10 +1,10 @@
 package com.flab.s_market.domains.term.repository;
 
 import com.flab.s_market.domains.term.domain.QSubTerm;
-import com.flab.s_market.domains.term.dto.response.DetailTermDTO;
-import com.querydsl.core.types.Projections;
+import com.flab.s_market.domains.term.domain.SubTerm;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import java.util.Optional;
 
 public class SubTermCustomRepositoryImpl implements SubTermCustomRepository{
 
@@ -15,18 +15,20 @@ public class SubTermCustomRepositoryImpl implements SubTermCustomRepository{
     }
 
     @Override
-    public DetailTermDTO findByTermIdAndVersion(Long termId, Integer version) {
+    public Optional<SubTerm> findByTermIdAndVersion(Long termId, Integer version) {
         QSubTerm subTerm = QSubTerm.subTerm;
 
-        return queryFactory
-            .select(Projections.constructor(
-                DetailTermDTO.class,
-                subTerm.url,
-                subTerm.id.version,
-                subTerm.term.title
-            ))
+        SubTerm sub = queryFactory
+            .select(subTerm)
             .from(subTerm)
             .where(subTerm.id.termId.eq(termId), subTerm.id.version.eq(version))
             .fetchOne();
+
+        return Optional.ofNullable(sub);
+        /*if(sub == null){
+            return Optional.empty();
+        }else{
+            return Optional.of(sub);
+        }*/
     }
 }
