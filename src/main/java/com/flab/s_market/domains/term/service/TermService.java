3a31a6a1f069967833e41deae7a2590ac8a776c1
songhee1 <ghenrhkwk88@naver.com>
2025-02-03 18:13:53 +0900

    [Style] 약관 전체목록 조회 api 코드 스타일 변경 (#1)
    - termController : 한번에 return 문에 service 호출로직까지 작성하지 않고 나누어 작성
    - termService : 스트림으로 같은 코드를 반복하지 않고 for loop로 한번만 작성하도록 변경

--- src/main/java/com/flab/s_market/domains/term/service/TermService.java
+++ src/main/java/com/flab/s_market/domains/term/service/TermService.java
@@ -7,6 +7,7 @@ import com.flab.s_market.domains.term.dto.response.TermResponseDTO;
 import com.flab.s_market.domains.term.dto.response.TermsResponseDTO;
 import com.flab.s_market.domains.term.repository.SubTermRepository;
 import com.flab.s_market.domains.term.repository.TermRepository;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,23 +19,20 @@ import org.springframework.transaction.annotation.Transactional;
 public class TermService {
 
     private final TermRepository termRepository;
-    private final SubTermRepository subTermRepository;
     private static final String mainTitle = "이용약관에 먼저 동의해주세요";
     private static final String additionalTitle = "신규 가입 회원에게만 드려요.\\n 유니버스 크럽 3개월 무료 이용!";
     public AllTermResponseDTO getAllTerms(){
-        List<Term> terms = termRepository.findAll();
+        List<Term> terms = termRepository.findAll(); // 쿼리 확인
+        List<TermResponseDTO> mainTermList = new ArrayList<>();
+        List<TermResponseDTO> additionalTermList = new ArrayList<>();
 
-        List<TermResponseDTO> mainTermList =
-            terms.stream()
-                .filter(term -> !term.getAdditional())
-                .map(TermResponseDTO::from)
-                .toList();
-
-        List<TermResponseDTO> additionalTermList =
-            terms.stream()
-                .filter(Term::getAdditional)
-                .map(TermResponseDTO::from)
-                .toList();
+        for(Term term : terms){
+            if(!term.getAdditional()){
+                mainTermList.add(TermResponseDTO.from(term));
+            }else{
+                additionalTermList.add(TermResponseDTO.from(term));
+            }
+        }
 
         TermsResponseDTO mainDto = TermsResponseDTO.from(mainTitle, mainTermList);
         TermsResponseDTO additionalDto = TermsResponseDTO.from(additionalTitle, additionalTermList);
@@ -43,7 +41,11 @@ public class TermService {
     }
 
     public DetailTermDTO getDetailTerms(Long termId, Integer version) {
-
-        return subTermRepository.findByTermIdAndVersion(termId, version);
+          return null;
+//        return subTermRepository.findByTermIdAndVersion(termId, version);
+//            subTermRepository.findByTermIdAndVersion(termId, version);
+//            .orElseThrow(() -> new CustomException(ErrorCode.NOT_EXIST_TERM, Map.of("termId", termId, "version", version),
+//                log::info));
+        // optional리턴해서 없으면 예외처리
     }
 }
